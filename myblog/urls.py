a95839aaa2d47ae9dc9d08645ef9420445403c1c
lofy <siiiokolad@mail.ru>
2020-04-14 17:51:45 +0300

    Init git heroku

---
+++ myblog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url('lofadmon/', admin.site.urls),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
