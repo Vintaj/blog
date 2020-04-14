adc8d935b094ac08be42319bcec5c011d621936b
lofy <siiiokolad@mail.ru>
2020-04-14 22:48:03 +0300

    Deploy

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -9,5 +9,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
+
+application = get_wsgi_application()
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
