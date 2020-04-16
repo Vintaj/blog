0b9a5f68020df72fd944efbb446d93ffa804fd53
lofy <siiiokolad@mail.ru>
2020-04-16 23:01:17 +0300

    2

--- myblog/settings.py
+++ myblog/settings.py
@@ -135,6 +135,6 @@ STATIC_URL = STATIC_HOST + '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = 'myblog/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
