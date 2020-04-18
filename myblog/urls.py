109b7cf80e56989f22ec2a6898f5596e10ca41d3
lofy <siiiokolad@mail.ru>
2020-04-19 00:12:10 +0300

    Восстанавливаем админку

--- myblog/urls.py
+++ myblog/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url('lofadmon/', admin.site.urls),
