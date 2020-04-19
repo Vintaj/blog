5db8a384023a1d27b291359ebd1d6036e2cf1694
lofy <siiiokolad@mail.ru>
2020-04-19 11:26:06 +0300

    Восстанавливаем админку4

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name='blog'
 urlpatterns = [
     # post views
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^blog', views.post_list, name='post_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
         r'(?P<post>[-\w]+)/$',
         views.post_detail,
